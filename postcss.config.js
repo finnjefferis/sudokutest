2cc350163780a93b46067787f6cf83fe91551da2
Finn Jefferis <91462667+finnjefferis@users.noreply.github.com>
2025-05-13 13:49:48 +0100

    removed unneccesary files

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: {
-      tailwindcss: {},                                    // run Tailwind
-      autoprefixer: {},                                   // vendor prefixes
-    },
-  };
-  
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class',                                   // ← CLASS–based dark-mode
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
-};
